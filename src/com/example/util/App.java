ead76a55ffaba4324101a74bd866b1eb71a03de9
fanyafeng <1181631922@qq.com>
2015-03-23 20:06:59 +0800

    测试cookie

---
+++ src/com/example/util/App.java
@@ -0,0 +1,12 @@
+package com.example.util;
+
+import android.app.Application;
+import android.content.Context;
+
+public class App extends Application{
+
+	public static Context mContext;
+	
+	
+	
+}
